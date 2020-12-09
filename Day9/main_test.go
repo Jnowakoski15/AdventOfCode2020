5c05f196fe5af6edf39faa155d4821dbe7cc160c
Jason Nowakoski <jaymanx15@gmail.com>
2020-12-09 08:39:18 -0500

    added real tests

--- Day9/main_test.go
+++ Day9/main_test.go
@@ -19,3 +19,21 @@ func Test2(t *testing.T) {
 		t.Errorf("Rez : %v does not match expected: %v\n", rez, exp)
 	}
 }
+
+func Test1Real(t *testing.T) {
+	exp := uint64(25918798)
+	rez := solvePart1("mainFile.txt", 25)
+
+	if rez != exp {
+		t.Errorf("Rez : %v does not match expected: %v\n", rez, exp)
+	}
+}
+
+func Test2Real(t *testing.T) {
+	exp := uint64(3340942)
+	rez := solvePart2("mainFile.txt", 25)
+
+	if rez != exp {
+		t.Errorf("Rez : %v does not match expected: %v\n", rez, exp)
+	}
+}
